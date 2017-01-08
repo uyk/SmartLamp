1941914479dae0ae0af236b34abafba34fd46a91
admin-PC\admin <meopulior@naver.com>
2017-01-09 00:51:55 +0900

    파이로부터 smartLamp GET 요청을 받는다

--- SmartLampServer/index.js
+++ SmartLampServer/index.js
@@ -20,16 +20,33 @@ var optionsTTS = {
     headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
 };
 
+var piIP = "http://218.150.183.144:5000";
+var optionsPi = {
+    uri : piIP + "/error",
+    method : "POST",
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    form : {
+    }
+};
+
 app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.static('public'));
 
 app.get('/', function (req,res) {
     console.log('app.get "/" ');
-    res.send('main');
 });
 
-app.get('/get', function (req,res) {
-    console.log('app.get /get');
-    res.send('get');
+//파이로부터 1초마다 호출
+app.get('/smartLamp', function(req,res) {
+    res.body = {
+        flag : 1,
+        colorR : 255,
+        colorG : 255,
+        colorB : 255
+    }
+    res.send("Test");
 });
 
 app.post('/facebook', function(req,res) {
@@ -37,6 +54,7 @@ app.post('/facebook', function(req,res) {
     var myMessage = req.body.myMessage;
     var updateAt = req.body.updateAt;
 
+    optionsPi.uri = piIP + "/facebook";
     longtext = "페이스북에" + req.body.from + "님이 " + req.body.myMessage + "라고 게시했습니다.";
     optionsTTS.form.text = longtext;
 
@@ -45,18 +63,15 @@ app.post('/facebook', function(req,res) {
     console.log('update at : ', updateAt );
     console.log('facebook post');
 
-    var writeStream = fs.createWriteStream('./tts1.mp3');
+    var writeStream = fs.createWriteStream('./public/tts1.mp3');
     var _req = request.post(optionsTTS).on('response', function(response) {
-        console.log(response.statusCode) // 200
+        console.log(response.statusCode) ;// 200
         console.log(response.headers['content-type'])
     });
     _req.pipe(writeStream); // file로 출력
 
-   fs.readFile("tts1.mp3");
-
     res.send("Success");
 });
-
 app.listen(port, function () {
     console.log('http://218.150.183.150:3000/tts app listening on port 3000!');
 });
