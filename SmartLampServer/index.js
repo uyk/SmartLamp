c2d3ed1dd4d41780762c2b7005811d52f9b18aab
admin-PC\admin <meopulior@naver.com>
2017-04-30 17:55:23 +0900

    라즈베리파이로 페이스북 이벤트 내용을 보낸다.

--- SmartLampServer/index.js
+++ SmartLampServer/index.js
@@ -1,7 +1,13 @@
+
 var express = require("express");
 var bodyParser = require("body-parser");
+
 var app = express();
 var port = 3000;
+
+var isData = false;
+var empty = "empty";
+var longtext = "";
 
 app.use(bodyParser.urlencoded({extended : false}));
 
@@ -10,19 +16,29 @@ app.get('/', function (req,res) {
     res.send("Smart Lamp Server");
 });
 
+//파이로부터 1초마다 호출됨
+app.get('/smartLamp', function(req,res) {
+    if(isData) {
+        isData = false;     //한번 데이터 출력한 후 초기화
+        res.send(longtext);
+    }
+    else {
+        res.send(empty);
+    }
+});
+
 app.post('/facebook', function(req,res) {
     var from = req.body.from;
     var myMessage = req.body.myMessage;
     var updateAt = req.body.updateAt;
 
-    console.log('from : ', from );
-    console.log('myMessage : ', myMessage );
-    console.log('update at : ', updateAt );
-    console.log('facebook post');
+    longtext = "페이스북에" + req.body.from + "님이" + req.body.myMessage + "라고 게시했습니다.";
+    console.log(longtext);
 
+    isData = true;
     res.send("Success");
 });
 
 app.listen(port, function () {
-    console.log('Smart Lamp Server app listening on port 3000!');
+    console.log('Smart Lamp Server app listening on port ' + port);
 });
